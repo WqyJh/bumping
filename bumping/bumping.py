cef247878dc70e04bd683ccb125a27a4fd07030a
WqyJh <781345688@qq.com>
2019-12-19 19:47:28 +0800

    feat: add commit range options

--- bumping/bumping.py
+++ bumping/bumping.py
@@ -16,13 +16,13 @@ import click
 
 from git import Repo
 
-from bumping import __version__
+from bumping import __version__, logger, set_debug
 
 
-_prog = re.compile(r'^([^\(\)]+?)(\([^\(\)]+\))?: [\s\S]+$')
+_COMMIT_TYPE = r'^([^\(\)]+?)(\([^\(\)]+\))?: [\s\S]+$'
 
 
-_increment_map = {
+_TYPE_INCREMENT = {
     'BREAKING CHANGE': (1, 0, 0),
     'feat': (0, 1, 0),
     'fix': (0, 0, 1),
@@ -30,13 +30,13 @@ _increment_map = {
 
 
 def get_commit_type(message: str) -> str:
-    m = _prog.match(message)
+    m = re.match(_COMMIT_TYPE, message)
     if m:
         return m.group(1)
 
 
 def get_increment(type: str) -> tuple:
-    return _increment_map.get(type, None)
+    return _TYPE_INCREMENT.get(type, None)
 
 
 def get_commit_increment(message: str) -> tuple:
@@ -53,30 +53,66 @@ def increase_version(v, inc):
         return (v[0], v[1], v[2] + inc[2])
 
 
-def get_commit_increments(repo, latest_rev='HEAD'):
-    t = (0, 0, 0)
-    for commit in repo.iter_commits(latest_rev):
-        # print(commit.name_rev)
+def _get_iter_rev(repo, starting_commit: str, stopping_commit: str):
+    if starting_commit:
+        c = repo.commit(starting_commit)
+        if not c.parents:
+            # starting_commit is initial commit,
+            # treat as default
+            starting_commit = ''
+
+    iter_rev = f'{starting_commit}..{stopping_commit}' if starting_commit else stopping_commit
+    return iter_rev
+
+
+def get_commit_increments(repo, base='', base_version=None, end='HEAD'):
+    t = parse_version(base_version or '0.0.0')
+    iter_rev = _get_iter_rev(repo, starting_commit=base, stopping_commit=end)
+    for commit in reversed(list(repo.iter_commits(iter_rev))):
         inc = get_commit_increment(commit.message)
         if inc:
+            logger.debug(commit.message)
             t = increase_version(t, inc)
     return t
 
 
-def parse_version(t: tuple) -> str:
+def parse_version(v):
+    '''Parse version string to version tuple.
+
+    :param v: version string like '0.0.1'
+    :returns a tuple contains three parts of the version like (0, 0, 1)
+    '''
+    try:
+        if v:
+            return tuple(map(int, v.split('.')))
+    except Exception:
+        pass
+
+    raise Exception(f'Invalid version string: {v}')
+
+
+def format_version(t: tuple) -> str:
     return '%d.%d.%d' % t
 
 
 @click.command()
 @click.version_option(__version__)
+@click.option('-d', '--debug', is_flag=True)
 @click.option('-r', '--repo', default='./',
               help="Path to the repository's root directory")
-def main(repo):
+@click.option('--base',
+              help='increase the version from base revision')
+@click.option('--base-version',
+              help='version number of base revision')
+@click.option('--end', default='HEAD',
+              help='increase the version to end revision')
+def main(debug, repo, base, base_version, end):
     '''Python tool to calculate SemVer based on conventional git commit messages.'''
+    set_debug(debug)
     repo = Repo(os.path.abspath(repo))
-
-    incs = get_commit_increments(repo)
-    print(parse_version(incs))
+    incs = get_commit_increments(
+        repo, base=base, base_version=base_version or base, end=end)
+    print(format_version(incs))
 
 
 if __name__ == '__main__':
